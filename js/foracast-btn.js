ef855395f9f8c343c175561a4bc76186ab95b46b
Kosmi2033 <kosmix.0912.ru@gmail.com>
2025-04-20 21:25:43 +0300

    20.04 21:25

--- js/foracast-btn.js
+++ js/foracast-btn.js
@@ -1,43 +1,19 @@
 'use strict'
 
-// Находим контейнер со слайдами
 const slides = document.getElementById('forecast__future-cards');
 
-// Определяем количество слайдов
-const slideCount = document.querySelectorAll('.forecast__future-cards-hourly').length;
-
-// Находим кнопки «Назад» и «Вперёд»
 const nextButton = document.getElementById('forecast__future-btn-weekly');
 const prevButton = document.getElementById('forecast__future-btn-hourly');
 
-let currentIndex = 0; // Переменная для хранения текущего индекса слайда
-let autoPlayInterval; // Переменная для хранения интервала автопрокрутки
-
-
-// @param {number} index 
-
-function goToSlide(index) {
-    if (index < 0) {
-        index = slideCount - 1; // Если текущий слайд первый, переходим на последний
-    } else if (index >= slideCount) {
-        index = 0; // Если текущий слайд последний, переходим на первый
-    }
-
-    currentIndex = index; // Запоминаем текущий индекс
-    slides.style.transform = `translateX(${-index * 100}%)`; // Смещаем контейнер слайдов
-    
-}
-
-// Добавляем обработчик клика для кнопки «Назад»
 prevButton.addEventListener('click', () => {
-    goToSlide(currentIndex + 1);
-    // slides.style.justifyContent = 'start'
+    slides.style.transform = `translateX(${0}%)`;
+    nextButton.classList.remove('forecast__future-btn--active');
+    prevButton.classList.add('forecast__future-btn--active');
 });
 
 // Добавляем обработчик клика для кнопки «Вперёд»
 nextButton.addEventListener('click', () => {
-    goToSlide(currentIndex - 1);
-    // slides.style.justifyContent = 'end'
+    slides.style.transform = `translateX(${-100}%)`;
+    prevButton.classList.remove('forecast__future-btn--active');
+    nextButton.classList.add('forecast__future-btn--active');
 });
-
-goToSlide(0);
