18e3dcc29445e0d8d117c1a9c8bd87737055fd67
Kosmi2033 <kosmix.0912.ru@gmail.com>
2025-04-22 01:19:43 +0300

    22.04 1:19

---
+++ js/add-btn.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function addCity(){
+    const citySect = document.getElementById('citySect')
+    if (citySect) {
+        citySect.style.left = '48.59vw';
+    } else {
+        console.log('Элемент с id="city" не найден');
+    }
+}
+
+function backAddCity() {
+    const citySect = document.getElementById('citySect')
+    if (citySect) {
+        citySect.style.left = '100%';
+    } else {
+        console.log('Элемент с id="city" не найден');
+    }
+}
+
+function addCard() {
+    const cardEl = document.querySelector('.city__card')
+    let cardsEl = document.querySelector('.city__cards')
+
+    cardsEl.innerHTML += cardEl.outerHTML
+}
