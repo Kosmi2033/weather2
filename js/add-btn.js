a9d036b26d11e7f1ba17fd9f8f634a95e7cbd1fc
Kosmi2033 <kosmix.0912.ru@gmail.com>
2025-04-30 18:44:11 +0300

    30.04 18:44

--- js/add-btn.js
+++ js/add-btn.js
@@ -2,8 +2,10 @@
 
 function addCity() {
     const citySect = document.getElementById('citySect')
+    let container = window.getComputedStyle(document.querySelector('.all-container')).width;
+    let procent = window.getComputedStyle(document.querySelector('.city__search')).width;
     if (citySect) {
-        citySect.style.left = '48.59vw';
+        citySect.style.left = Math.round(parseInt(container.slice(0, container.length - 2)) - parseInt(procent.slice(0, procent.length - 2))) + 'px';
         document.getElementById('favoriteCitySect').style.left = '100%';
     } else {
         console.log('Элемент с id="city" не найден');
@@ -21,19 +23,6 @@ function backAddCity() {
     }
 }
 
-// function addCard() {
-//     const cityCard = document.querySelector('.city__card');
-//     const error1 = document.getElementById('city__card-error-search');
-//     const card1 = cityCard.cloneNode(true); // Создаем копию текущего элемента
-//     const cards = document.querySelector('#cityFavoriteCards');
-
-//     // Изменяем изображение в копии
-//     card1.querySelector('#imgCard').src = './img/main-page/stars/star-after.png';
-//     card1.querySelector('#imgCard').setAttribute('onclick','delCard(this)')
-
-//     cards.appendChild(card1); // Добавляем копию в целевой блок
-// }
-
 function delCard(element) {
     const card = element.closest('.city__card');
     const cards = document.querySelector('#cityFavoriteCards');
@@ -43,13 +32,14 @@ function delCard(element) {
     } else {
         console.error('Элемент city_card не найден');
     }
-
 }
 
 function favoriteCity() {
     const citySect = document.getElementById('favoriteCitySect')
+    let container = window.getComputedStyle(document.querySelector('.all-container')).width;
+    let procent = window.getComputedStyle(document.querySelector('.forecast')).width;
     if (citySect) {
-        citySect.style.left = '48.59vw';
+        citySect.style.left = Math.round(parseInt(container.slice(0, container.length - 2)) - parseInt(procent.slice(0, procent.length - 2))) + 'px';
         document.getElementById('citySect').style.left = '100%';
     } else {
         console.log('Элемент с id="city" не найден');
